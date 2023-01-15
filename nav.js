a6c8f238f03651891cecc7f6abff724ff8142074
merehanyehia <merehan0yehia@gmail.com>
2023-01-16 01:21:59 +0200

    last commit

---
+++ nav.js
@@ -0,0 +1,18 @@
+// $("#button").click(function(){
+//     $(".navy").toggleClass("hide");
+//     $("i").toggleClass("active");
+//   });
+
+
+$("#button").click(function(){
+    $(".navy").toggle();
+  });
+
+// $(document).ready(function() {
+//     $("#button").sideToggle({
+//      moving: '.navy',
+//      direction: 'right'
+
+//     });
+//     });
+
